95d1d0ce6378efc8d69c3090791f3deed065fc2d
Pedro Henrick <engsoftwarepedro@gmail.com>
2025-04-28 20:46:25 -0300

    Update arvore_binaria.py

--- ArvoresBinarias/arvore_binaria.py
+++ ArvoresBinarias/arvore_binaria.py
@@ -1,1 +1,34 @@
+class NoArvore:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esquerda = None
+        self.direita = None
+
+class ArvoreBinaria:
+    def __init__(self):
+        self.raiz = None
+
+    def inserir(self, valor):
+        if self.raiz is None:
+            self.raiz = NoArvore(valor)
+        else:
+            self._inserir(self.raiz, valor)
+
+    def _inserir(self, no, valor):
+        if valor < no.valor:
+            if no.esquerda is None:
+                no.esquerda = NoArvore(valor)
+            else:
+                self._inserir(no.esquerda, valor)
+        else:
+            if no.direita is None:
+                no.direita = NoArvore(valor)
+            else:
+                self._inserir(no.direita, valor)
+
+    def em_ordem(self, no):
+        if no:
+            self.em_ordem(no.esquerda)
+            print(no.valor)
+            self.em_ordem(no.direita)
 
